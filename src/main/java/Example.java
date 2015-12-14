c675d8b7fcd99ac3fcffe74041c54c2e2da6bedd
Wusung Peng <wusungpeng@kkbox.com>
2015-12-14 09:44:02 +0800

    Initial commit

---
+++ src/main/java/Example.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wusungpeng
+ * @since 12/14/15
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
